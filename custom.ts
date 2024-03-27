1b7c92436b4927baa31123b01fb88e9d130b15ef
Miyayu <yuchan2215@gmail.com>
2024-03-27 20:38:54 +0900

    add: 木のピッケルを右クリックで開けるように

--- custom.ts
+++ custom.ts
@@ -52,6 +52,16 @@ let tireType: TireType | null = null
  */
 //% weight=100 color=#0fbc11 icon="\uf1b9" block="カーディーラー"
 namespace custom {
+
+    let isIntracted: Boolean = false
+    function enableIntract(handler: () => void) {
+        player.onItemInteracted(WOODEN_PICKAXE, function () {
+            if (isIntracted === false) {
+                isIntracted = true;
+                handler();
+            }
+        })
+    }
     function animation(startX: number, startY: number, startZ: number) {
         agent.teleport(world(startX, startY, startZ), NORTH)
         agent.move(RIGHT, 7)
@@ -76,80 +86,92 @@ namespace custom {
     //% blockId=whenStartZero
     //% block=くるまをつくるとき
     export function whenStartZero(handler: () => void): void {
-        animation(1216, 64, 1263);
-        player.execute("scoreboard players add @s phase 1")
+        enableIntract(function(){
+            animation(1216, 64, 1263);
+            player.execute("scoreboard players add @s phase 1")
+        })
     }
 
     //% blockId=whenStartOne
     //% block=くるまをつくるとき
     export function whenStartOne(handler: () => void): void {
-        handler();
-        if (
-            carType == CarType.Sedan,
-            bodyColor == BodyColor.White,
-            tireType == TireType.Silver
-        ) {
-            animation(1218, 64, 1255);
-            player.execute("scoreboard players add @s phase 1")
-        } else {
-            player.execute("scoreboard players add @s phase 2")
-        }
+        enableIntract(function () {
+            handler();
+            if (
+                carType == CarType.Sedan,
+                bodyColor == BodyColor.White,
+                tireType == TireType.Silver
+            ) {
+                animation(1218, 64, 1255);
+                player.execute("scoreboard players add @s phase 1")
+            } else {
+                player.execute("scoreboard players add @s phase 2")
+            }
+        })
     }
 
     //% blockId=whenStartTwo
     //% block=くるまをつくるとき
     export function whenStartTwo(handler: () => void): void {
-        handler();
-        if (
-            carType == CarType.Wagon,
-            bodyColor == BodyColor.Blue,
-            tireType == TireType.Yellow
-        ) {
-            animation(1218, 64, 1246);
-            player.execute("scoreboard players add @s phase 1")
-        } else {
-            player.execute("scoreboard players add @s phase 2")
-        }
+        enableIntract(function () {
+            handler();
+            if (
+                carType == CarType.Wagon,
+                bodyColor == BodyColor.Blue,
+                tireType == TireType.Yellow
+            ) {
+                animation(1218, 64, 1246);
+                player.execute("scoreboard players add @s phase 1")
+            } else {
+                player.execute("scoreboard players add @s phase 2")
+            }
+        })
     }
 
     //% blockId=whenStartThree
     //% block=くるまをつくるとき
     export function whenStartThree(handler: () => void): void {
-        handler();
-        if (
-            carType == CarType.Compact,
-            bodyColor == BodyColor.Yellow,
-            tireType == TireType.Yellow
-        ) {
-            animation(1218, 64, 1237);
-            player.execute("scoreboard players add @s phase 1")
-        } else {
-            player.execute("scoreboard players add @s phase 2")
-        }
+        enableIntract(function () {
+            handler();
+            if (
+                carType == CarType.Compact,
+                bodyColor == BodyColor.Yellow,
+                tireType == TireType.Yellow
+            ) {
+                animation(1218, 64, 1237);
+                player.execute("scoreboard players add @s phase 1")
+            } else {
+                player.execute("scoreboard players add @s phase 2")
+            }
+        })
     }
 
     //% blockId=whenStartFour
     //% block=くるまをつくるとき
     export function whenStartFour(handler: () => void): void {
-        handler();
-        if (
-            carType == CarType.Sports,
-            bodyColor == BodyColor.Black,
-            tireType == TireType.Black
-        ) {
-            animation(1218, 64, 1228);
-            player.execute("scoreboard players add @s phase 1")
-        } else {
-            player.execute("scoreboard players add @s phase 2")
-        }
+        enableIntract(function () {
+            handler();
+            if (
+                carType == CarType.Sports,
+                bodyColor == BodyColor.Black,
+                tireType == TireType.Black
+            ) {
+                animation(1218, 64, 1228);
+                player.execute("scoreboard players add @s phase 1")
+            } else {
+                player.execute("scoreboard players add @s phase 2")
+            }
+        })
     }
 
     //% blockId=whenStartFree
     //% block=くるまをつくるとき
     export function whenStartFree(handler: () => void): void {
-        handler();
-        animation(1218, 64, 1215);
-        player.execute("scoreboard players add @s phase 1")
+        enableIntract(function () {
+            handler();
+            animation(1218, 64, 1215);
+            player.execute("scoreboard players add @s phase 1")
+        })
     }
 
     //% blockId=whenStartDummy
